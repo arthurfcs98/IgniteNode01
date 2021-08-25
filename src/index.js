e0717deb64329486fa1f8ce129ca49089ad052cb
Arthur Cesarino <cesarinoarthur@gmail.com>
2021-08-25 18:51:01 -0300

    test todos sucesso

--- src/index.js
+++ src/index.js
@@ -22,9 +22,10 @@ function checksExistsUserAccount(request, response, next) {
 
   if (!user) return response.status(400).json(error);
 
+  console.log(user);
   request.user = user;
 
-  return response.status(201).json(user);
+  return next();
 }
 
 app.post("/users", (request, response) => {
@@ -49,22 +50,77 @@ app.post("/users", (request, response) => {
 
 app.get("/todos", checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const { user } = request;
+
+  return response.status(200).json(user.todos);
 });
 
 app.post("/todos", checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const { user } = request;
+
+  const { title, deadline } = request.body;
+
+  const todo = {
+    id: uuidv4(), // precisa ser um uuid
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date(),
+  };
+
+  user.todos.push(todo);
+
+  return response.status(201).json(todo);
 });
 
 app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const { id } = request.params;
+  const { title, deadline } = request.body;
+  const { user } = request;
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) return response.status(404).json(error);
+
+  todo.title = title;
+  todo.done = false;
+  todo.deadline = new Date(deadline);
+
+  return response.json({
+    title,
+    done: false,
+    deadline: todo.deadline,
+  });
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const { id } = request.params;
+  const { user } = request;
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) return response.status(404).json(error);
+
+  todo.done = true;
+
+  return response.json(todo);
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const { id } = request.params;
+  const { user } = request;
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) return response.status(404).json(error);
+
+  user.todos.splice(todo, 1);
+
+  return response.status(204).send();
 });
 
 module.exports = app;
